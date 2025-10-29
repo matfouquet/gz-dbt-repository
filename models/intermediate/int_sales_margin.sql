8a82683c59601b2a12893e2b30c1900f5a7d4de6
Mathilde Fouquet <mathildefouquet24@gmail.com>
2025-10-29 14:32:13 +0000

    calculated purchase cost and margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,22 @@
+WITH produc_sales_join AS (
+SELECT
+orders_id
+,products_id
+,date_date
+,revenue
+,quantity
+,quantity * purchase_price AS purchase_cost
+FROM {{ ref('stg_raw__sales') }}
+INNER JOIN {{ ref('stg_raw__product') }}
+USING (products_id)
+)
+SELECT
+*
+,revenue - purchase_cost AS margin
+FROM produc_sales_join
+
+
+
+
+
+
